6478f0a5d195c0a573ee66e9cb4ce2e84bd9e3c5
Ivan <i.v.a.n_l@mail.ru>
2020-05-12 23:15:33 +0300

    fix

--- alexa_event_listeners.py
+++ alexa_event_listeners.py
@@ -10,14 +10,14 @@ async def main():
     handlers_stat = {}
 
     top100_urls = []
-    with open('../alexa_top100.txt') as f:
+    with open('alexa_top100.txt') as f:
         for line in f:
             line = "https://" + line.strip() + '/'
             top100_urls.append(line)
 
     api = await (DynamicAPI().init())
 
-    for u in top100_urls[:2]:
+    for u in top100_urls[:10]:
         urls_queue.put(u)
 
     while not urls_queue.empty():
@@ -28,14 +28,14 @@ async def main():
     
         curr_handlers_stat = defaultdict(int)
         for result in results:
-            ev_handlers = result["eventHandlers"]
+            ev_handlers = result["eventListeners"]
             for lst in ev_handlers:
                 for dct in lst:
                     curr_handlers_stat[dct['type']] += 1
         handlers_stat[url] = curr_handlers_stat
 
-        with open('../top100_handlers.txt', 'w') as f:
-            json.dump(handlers_stat, f)
+    with open('../top100_handlers.txt', 'w') as f:
+        json.dump(handlers_stat, f)
     await api.destroy()
 
 
